02be1ecfcd88583b54046ec199e34e02637465e4
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-10-11 13:37:21 +0530

    Update DBConnTest.py

--- DBConnTest.py
+++ DBConnTest.py
@@ -1,1 +1,14 @@
+import pyodbc
+server = 'techitoutserver.database.windows.net'
+database = 'techitoutdb'
+username = 'techitoutadmin'
+password = '{Cloudmoyo@123}'
+driver= '{ODBC Driver 18 for SQL Server}'
 
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM sys.databases")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
